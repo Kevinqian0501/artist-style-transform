a563b296437c6c876606e3f0d8f807f987b489fe
Liuqian0501 <liu.qian.am@gmail.com>
2017-07-18 13:13:23 -0500

    firebase support

--- Google-web-app/config.py
+++ Google-web-app/config.py
@@ -1,3 +1,4 @@
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 PREDICTION_SERVICE_URL = 'http://35.197.11.221:5555'
+FIREBASE_URL = 'https://realtimestyletrans.firebaseio.com/'
