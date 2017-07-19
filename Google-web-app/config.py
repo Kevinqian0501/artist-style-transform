78259ad3a3c6be2ccee2220aa754b50e83abd251
Liuqian0501 <liu.qian.am@gmail.com>
2017-07-18 23:48:14 -0500

    port

--- Google-web-app/config.py
+++ Google-web-app/config.py
@@ -1,4 +1,4 @@
 MAX_CONTENT_LENGTH = 8 * 1024 * 1024
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
-PREDICTION_SERVICE_URL = 'http://35.197.11.221:5555'
+PREDICTION_SERVICE_URL = 'http://35.197.11.221:8080'
 FIREBASE_URL = 'https://realtimestyletrans.firebaseio.com/'
